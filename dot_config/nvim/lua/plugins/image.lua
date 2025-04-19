3108e065760b048310cfee3afb53f7fdd81279e2
Eric Lin <ttttt123465@gmail.com>
2025-04-19 20:30:15 +0800

    add image lua

---
+++ dot_config/nvim/lua/plugins/image.lua
@@ -0,0 +1,10 @@
+return {
+  "3rd/image.nvim",
+  opts = {
+    -- You can add options here, or leave empty for defaults
+    -- Example: show images only on hover
+    -- render = { min_padding = 5, show_label = true },
+  },
+  -- Uncomment if you use luarocks.nvim for magick dependency:
+  -- dependencies = { "vhyrro/luarocks.nvim" },
+}
