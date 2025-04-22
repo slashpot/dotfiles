f689e40d547bb86513e7849ab07022363933d9bc
Eric Lin <ttttt123465@gmail.com>
2025-04-22 12:04:13 +0800

    update nvim config

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -37,23 +37,23 @@ return {
     vim.cmd([[Neotree close]])
   end,
   init = function()
-    -- FIX: use `autocmd` for lazy-loading neo-tree instead of directly requiring it,
-    -- because `cwd` is not set up properly.
-    vim.api.nvim_create_autocmd("BufEnter", {
-      group = vim.api.nvim_create_augroup("Neotree_start_directory", { clear = true }),
-      desc = "Start Neo-tree with directory",
-      once = true,
-      callback = function()
-        if package.loaded["neo-tree"] then
-          return
-        else
-          local stats = vim.uv.fs_stat(vim.fn.argv(0))
-          if stats and stats.type == "directory" then
-            require("neo-tree")
-          end
+  -- FIX: use `autocmd` for lazy-loading neo-tree instead of directly requiring it,
+  -- because `cwd` is not set up properly.
+  vim.api.nvim_create_autocmd("BufEnter", {
+    group = vim.api.nvim_create_augroup("Neotree_start_directory", { clear = true }),
+    desc = "Start Neo-tree with directory",
+    once = true,
+    callback = function()
+      if package.loaded["neo-tree"] then
+        return
+      else
+        local stats = vim.uv.fs_stat(vim.fn.argv(0))
+        if stats and stats.type == "directory" then
+          require("neo-tree")
         end
-      end,
-    })
+      end
+    end,
+  })
   end,
   opts = {
     sources = { "filesystem", "buffers", "git_status" },
@@ -65,9 +65,11 @@ return {
       filtered_items = {
         visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
         hide_dotfiles = false,
-      }
+      },
+      hijack_netrw_behavior = "open_current"
     },
     window = {
+      position = 'float',
       mappings = {
         ["l"] = "open",
         ["h"] = "close_node",
